f01d8e653889dc5cfcc800b4a86fd809beef2f19
Jeremie <Jeremie@DESKTOP-14UQB2M>
2018-05-31 22:27:52 +0700

    duy3

---
+++ QuanLyThuVien1/QLyBUS/laptheBUS.vb
@@ -0,0 +1,64 @@
+﻿Imports QLyDTO
+Imports QLyDAL
+
+
+Public Class LaptheBUS
+    Public Function ValidReaderName(lt As LaptheDTO) As Boolean
+
+        If (lt.ReaderName.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidReaderID(lt As LaptheDTO) As Boolean
+
+        If (lt.ReaderID.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidAdress(lt As LaptheDTO) As Boolean
+
+        If (lt.Adress.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidReaderCategoryID(lt As QLyDTO.LaptheDTO) As Boolean
+
+        If (lt.ReaderCategoryID.ToString().Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidBirthday(lt As QLyDTO.LaptheDTO) As Boolean
+
+        If (lt.Birthday.ToString().Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidDayOfCard(lt As QLyDTO.LaptheDTO) As Boolean
+
+        If (lt.DayOfCard.ToString.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+    Public Function ValidEmail(lt As QLyDTO.LaptheDTO) As Boolean
+
+        If (lt.Email.Length < 1) Then
+            Return False
+        End If
+
+        Return True
+    End Function
+
+
+End Class
--- QuanLyThuVien1/QLyGUI/frmLapthe.vb
+++ QuanLyThuVien1/QLyGUI/frmLapthe.vb
@@ -1,3 +1,60 @@
-﻿Public Class frmLapthe
+﻿Imports QLyBUS
+Imports QLyDAL
+Imports QLyDTO
 
+Public Class frmLapthe
+
+    Private Sub btLapthe_Click_1(sender As Object, e As EventArgs) Handles btLapthe.Click
+
+        Dim lt As LaptheDTO
+        lt = New LaptheDTO()
+
+        '1. Lop GUI control
+        lt.ReaderID = tbMaDocGia.Text
+        lt.ReaderName = tbTenDocGia.Text
+        lt.ReaderCategoryID = cbLoaiDocGia.Text
+        lt.Birthday = dtpNgaySinh.Text
+        lt.Adress = tbDiaChi.Text
+        lt.DayOfCard = dtpNgayLapThe.Text
+        lt.Email = tbEmail.Text
+        '2. Business .... kiem tra cac textbox du lieu nhap
+        Dim ltbus As New LaptheBUS
+        If (ltbus.ValidReaderName(lt) = False) Then
+            MessageBox.Show("Ho ten chua duoc nhap")
+            lt.ReaderName()
+            Return
+        End If
+
+        If (ltbus.ValidAdress(lt) = False) Then
+            MessageBox.Show("Dia chi chua duoc nhap")
+            tbHoTen.Focus()
+            Return
+        End If
+
+        If (ltbus.isValidClass(lt) = False) Then
+            MessageBox.Show("Lop chua duoc nhap")
+            tbHoTen.Focus()
+            Return
+        End If
+        If (ltbus.isValidAddress(lt) = False) Then
+            MessageBox.Show("Dia chi chua duoc nhap")
+            tbHoTen.Focus()
+            Return
+        End If
+
+        '3. Chen ket qua vao DataBase
+        Dim ltdal As QLyDAL.LaptheDAL
+        ltdal = New QLyDAL.LaptheDAL()
+        Dim result As Integer
+        result = ltdal.insertDocGia(lt)
+        If (result = 0) Then
+            MessageBox.Show("Them hoc sinh thanh cong.")
+        Else
+            MessageBox.Show("Them hoc sinh that bai.")
+        End If
+    End Sub
+
+    Private Sub frmLapthe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
