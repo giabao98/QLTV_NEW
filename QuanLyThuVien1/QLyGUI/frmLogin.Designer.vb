6c0fb7aa91dfe0d3c605c387268ad94d0ab73454
BẢO TRẦN GIA <16520088@ms.uit.edu.vn>
2018-05-31 20:44:23 +0700

    edited 2

--- QuanLyThuVien1/QLyGUI/frmLogin.Designer.vb
+++ QuanLyThuVien1/QLyGUI/frmLogin.Designer.vb
@@ -22,10 +22,102 @@ Partial Class frmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.txbPassword = New System.Windows.Forms.TextBox()
+        Me.txbUserName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BunifuTileButton1 = New Bunifu.Framework.UI.BunifuTileButton()
+        Me.SuspendLayout()
+        '
+        'txbPassword
+        '
+        Me.txbPassword.Location = New System.Drawing.Point(52, 262)
+        Me.txbPassword.Multiline = True
+        Me.txbPassword.Name = "txbPassword"
+        Me.txbPassword.Size = New System.Drawing.Size(318, 33)
+        Me.txbPassword.TabIndex = 9
+        '
+        'txbUserName
+        '
+        Me.txbUserName.Location = New System.Drawing.Point(52, 154)
+        Me.txbUserName.Multiline = True
+        Me.txbUserName.Name = "txbUserName"
+        Me.txbUserName.Size = New System.Drawing.Size(318, 34)
+        Me.txbUserName.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(48, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 24)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Mật khẩu:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(48, 127)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(146, 24)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Tên đăng nhập:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(134, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(181, 32)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "ĐĂNG NHẬP"
+        '
+        'BunifuTileButton1
+        '
+        Me.BunifuTileButton1.BackColor = System.Drawing.Color.SeaGreen
+        Me.BunifuTileButton1.color = System.Drawing.Color.SeaGreen
+        Me.BunifuTileButton1.colorActive = System.Drawing.Color.MediumSeaGreen
+        Me.BunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BunifuTileButton1.Font = New System.Drawing.Font("Century Gothic", 15.75!)
+        Me.BunifuTileButton1.ForeColor = System.Drawing.Color.White
+        Me.BunifuTileButton1.Image = CType(resources.GetObject("BunifuTileButton1.Image"), System.Drawing.Image)
+        Me.BunifuTileButton1.ImagePosition = 20
+        Me.BunifuTileButton1.ImageZoom = 50
+        Me.BunifuTileButton1.LabelPosition = 41
+        Me.BunifuTileButton1.LabelText = "Tile 1"
+        Me.BunifuTileButton1.Location = New System.Drawing.Point(141, 317)
+        Me.BunifuTileButton1.Margin = New System.Windows.Forms.Padding(6)
+        Me.BunifuTileButton1.Name = "BunifuTileButton1"
+        Me.BunifuTileButton1.Size = New System.Drawing.Size(173, 54)
+        Me.BunifuTileButton1.TabIndex = 10
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(435, 432)
+        Me.Controls.Add(Me.BunifuTileButton1)
+        Me.Controls.Add(Me.txbPassword)
+        Me.Controls.Add(Me.txbUserName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLogin"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txbPassword As TextBox
+    Friend WithEvents txbUserName As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BunifuTileButton1 As Bunifu.Framework.UI.BunifuTileButton
 End Class
