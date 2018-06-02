17a5d89249c8e879ac11e8c8dd3f1873bf63e0fe
BẢO TRẦN GIA <16520088@ms.uit.edu.vn>
2018-06-03 05:18:37 +0700

    edited 11

--- QuanLyThuVien1/QLyGUI/frmMain.vb
+++ QuanLyThuVien1/QLyGUI/frmMain.vb
@@ -3,9 +3,7 @@
     Private LoginSign As DialogResult
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim flogin = New frmLogin()
-        Dim lt = New frmLapthe()
         flogin.ShowDialog()
-        'lt.ShowDialog()
 
         ''Kiem Tra ket qua Login, neu khong dung thi close
         LoginSign = flogin.DialogResult
@@ -16,4 +14,34 @@
 
     End Sub
 
+    Private Sub btnBookUpdate_Click(sender As Object, e As EventArgs) Handles btnBookUpdate.Click
+        Dim fLapThe = New frmLapthe
+        fLapThe.ShowDialog()
+    End Sub
+
+    Private Sub btnReaderManagement_Click(sender As Object, e As EventArgs) Handles btnReaderManagement.Click
+
+    End Sub
+
+    Private Sub btnAuthorManagement_Click(sender As Object, e As EventArgs) Handles btnAuthorManagement.Click
+
+    End Sub
+
+    Private Sub btnBorrowBook_Click(sender As Object, e As EventArgs) Handles btnBorrowBook.Click
+
+    End Sub
+
+    Private Sub btnFindBook_Click(sender As Object, e As EventArgs) Handles btnFindBook.Click
+
+    End Sub
+
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        'Me.Controls.Clear()
+        'InitializeComponent()
+        'frmMain_Load(e, e)
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Application.Exit()
+    End Sub
 End Class
