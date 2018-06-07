a04ddec43295543449cede5e73578287e6464c2f
BẢO TRẦN GIA <16520088@ms.uit.edu.vn>
2018-06-08 01:56:54 +0700

    edited 18

--- QuanLyThuVien1/QLyGUI/frmMain.Designer.vb
+++ QuanLyThuVien1/QLyGUI/frmMain.Designer.vb
@@ -32,14 +32,12 @@ Partial Class frmMain
         Me.btnClose = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btnBookUpdate = New System.Windows.Forms.Button()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnReaderManagement
@@ -47,7 +45,7 @@ Partial Class frmMain
         Me.btnReaderManagement.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnReaderManagement.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnReaderManagement.Image = CType(resources.GetObject("btnReaderManagement.Image"), System.Drawing.Image)
-        Me.btnReaderManagement.Location = New System.Drawing.Point(201, 31)
+        Me.btnReaderManagement.Location = New System.Drawing.Point(203, 21)
         Me.btnReaderManagement.Name = "btnReaderManagement"
         Me.btnReaderManagement.Size = New System.Drawing.Size(182, 126)
         Me.btnReaderManagement.TabIndex = 1
@@ -61,7 +59,7 @@ Partial Class frmMain
         Me.btnAuthorManagement.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnAuthorManagement.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnAuthorManagement.Image = CType(resources.GetObject("btnAuthorManagement.Image"), System.Drawing.Image)
-        Me.btnAuthorManagement.Location = New System.Drawing.Point(6, 176)
+        Me.btnAuthorManagement.Location = New System.Drawing.Point(8, 166)
         Me.btnAuthorManagement.Name = "btnAuthorManagement"
         Me.btnAuthorManagement.Size = New System.Drawing.Size(182, 126)
         Me.btnAuthorManagement.TabIndex = 2
@@ -75,7 +73,7 @@ Partial Class frmMain
         Me.btnBorrowBook.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnBorrowBook.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnBorrowBook.Image = CType(resources.GetObject("btnBorrowBook.Image"), System.Drawing.Image)
-        Me.btnBorrowBook.Location = New System.Drawing.Point(401, 31)
+        Me.btnBorrowBook.Location = New System.Drawing.Point(403, 21)
         Me.btnBorrowBook.Name = "btnBorrowBook"
         Me.btnBorrowBook.Size = New System.Drawing.Size(182, 126)
         Me.btnBorrowBook.TabIndex = 3
@@ -89,7 +87,7 @@ Partial Class frmMain
         Me.btnFindBook.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnFindBook.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnFindBook.Image = CType(resources.GetObject("btnFindBook.Image"), System.Drawing.Image)
-        Me.btnFindBook.Location = New System.Drawing.Point(201, 176)
+        Me.btnFindBook.Location = New System.Drawing.Point(203, 166)
         Me.btnFindBook.Name = "btnFindBook"
         Me.btnFindBook.Size = New System.Drawing.Size(182, 126)
         Me.btnFindBook.TabIndex = 4
@@ -103,7 +101,7 @@ Partial Class frmMain
         Me.btnLogout.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnLogout.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLogout.Image = CType(resources.GetObject("btnLogout.Image"), System.Drawing.Image)
-        Me.btnLogout.Location = New System.Drawing.Point(11, 21)
+        Me.btnLogout.Location = New System.Drawing.Point(403, 314)
         Me.btnLogout.Name = "btnLogout"
         Me.btnLogout.Size = New System.Drawing.Size(182, 127)
         Me.btnLogout.TabIndex = 5
@@ -127,7 +125,7 @@ Partial Class frmMain
         Me.btnClose.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnClose.Image = CType(resources.GetObject("btnClose.Image"), System.Drawing.Image)
-        Me.btnClose.Location = New System.Drawing.Point(11, 164)
+        Me.btnClose.Location = New System.Drawing.Point(403, 465)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(182, 126)
         Me.btnClose.TabIndex = 7
@@ -151,7 +149,7 @@ Partial Class frmMain
         Me.btnBookUpdate.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.btnBookUpdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnBookUpdate.Image = CType(resources.GetObject("btnBookUpdate.Image"), System.Drawing.Image)
-        Me.btnBookUpdate.Location = New System.Drawing.Point(6, 31)
+        Me.btnBookUpdate.Location = New System.Drawing.Point(8, 21)
         Me.btnBookUpdate.Name = "btnBookUpdate"
         Me.btnBookUpdate.Size = New System.Drawing.Size(182, 126)
         Me.btnBookUpdate.TabIndex = 0
@@ -160,42 +158,12 @@ Partial Class frmMain
         Me.btnBookUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
         Me.btnBookUpdate.UseVisualStyleBackColor = False
         '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.Button1)
-        Me.GroupBox1.Controls.Add(Me.btnReaderManagement)
-        Me.GroupBox1.Controls.Add(Me.btnBookUpdate)
-        Me.GroupBox1.Controls.Add(Me.btnAuthorManagement)
-        Me.GroupBox1.Controls.Add(Me.btnBorrowBook)
-        Me.GroupBox1.Controls.Add(Me.btnFindBook)
-        Me.GroupBox1.Location = New System.Drawing.Point(22, 81)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(591, 311)
-        Me.GroupBox1.TabIndex = 9
-        Me.GroupBox1.TabStop = False
-        '
-        'MonthCalendar1
-        '
-        Me.MonthCalendar1.Location = New System.Drawing.Point(83, 395)
-        Me.MonthCalendar1.Name = "MonthCalendar1"
-        Me.MonthCalendar1.TabIndex = 0
-        '
-        'GroupBox2
-        '
-        Me.GroupBox2.Controls.Add(Me.btnClose)
-        Me.GroupBox2.Controls.Add(Me.btnLogout)
-        Me.GroupBox2.Location = New System.Drawing.Point(411, 389)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(202, 298)
-        Me.GroupBox2.TabIndex = 10
-        Me.GroupBox2.TabStop = False
-        '
         'Button1
         '
         Me.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.Location = New System.Drawing.Point(401, 176)
+        Me.Button1.Location = New System.Drawing.Point(403, 166)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(182, 126)
         Me.Button1.TabIndex = 5
@@ -204,13 +172,37 @@ Partial Class frmMain
         Me.Button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
         Me.Button1.UseVisualStyleBackColor = False
         '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(68, 304)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 0
+        '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(22, 619)
+        Me.TextBox1.Location = New System.Drawing.Point(14, 526)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(383, 68)
+        Me.TextBox1.Size = New System.Drawing.Size(383, 65)
         Me.TextBox1.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.MonthCalendar1)
+        Me.GroupBox1.Controls.Add(Me.btnClose)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.btnLogout)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btnBookUpdate)
+        Me.GroupBox1.Controls.Add(Me.btnFindBook)
+        Me.GroupBox1.Controls.Add(Me.btnReaderManagement)
+        Me.GroupBox1.Controls.Add(Me.btnBorrowBook)
+        Me.GroupBox1.Controls.Add(Me.btnAuthorManagement)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 78)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(591, 609)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
         '
         'frmMain
         '
@@ -218,9 +210,6 @@ Partial Class frmMain
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.ClientSize = New System.Drawing.Size(1062, 710)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.MonthCalendar1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
@@ -228,7 +217,7 @@ Partial Class frmMain
         Me.Text = "frmMain"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -242,9 +231,8 @@ Partial Class frmMain
     Friend WithEvents btnClose As Button
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents btnBookUpdate As Button
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Button1 As Button
     Friend WithEvents MonthCalendar1 As MonthCalendar
-    Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
