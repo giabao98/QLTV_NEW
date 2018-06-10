ce70cab4153c30d6456c87cecc654f534ee7d32e
BẢO TRẦN GIA <16520088@ms.uit.edu.vn>
2018-06-10 23:08:17 +0700

    edited 23

--- QuanLyThuVien1/QLyBUS/FindBookBUS.vb
+++ QuanLyThuVien1/QLyBUS/FindBookBUS.vb
@@ -1,3 +1,73 @@
-﻿Public Class FindBookBUS
+﻿Imports QLyDTO
 
+Public Class FindBookBUS
+    Public Function ValidKindOfFind(fb As FindBookDTO) As Boolean
+        If (fb.Kind.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidFind(fb As FindBookDTO) As Boolean
+        If (fb.Find.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidBookCode(fb As FindBookDTO) As Boolean
+        If (fb.BookCode.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidBookName(fb As FindBookDTO) As Boolean
+        If (fb.BookName.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidProducerCode(fb As FindBookDTO) As Boolean
+        If (fb.ProducerCode.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidAuthorCode(fb As FindBookDTO) As Boolean
+        If (fb.AuthorCode.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidKindCode(fb As FindBookDTO) As Boolean
+        If (fb.KindCode.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidPublishingYear(fb As FindBookDTO) As Boolean
+        If (fb.PublishingYear.ToString < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidAmount(fb As FindBookDTO) As Boolean
+        If (fb.Amount.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidInputDay(fb As FindBookDTO) As Boolean
+        If (fb.InputDay.ToString.Length < 1) Then
+            Return False
+        End If
+        Return True
+    End Function
 End Class
--- QuanLyThuVien1/QLyGUI/frmFindBook.vb
+++ QuanLyThuVien1/QLyGUI/frmFindBook.vb
@@ -1,6 +1,15 @@
-﻿Public Class frmFindBook
+﻿Imports QLyBUS
+
+Public Class frmFindBook
     Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
 
     End Sub
 
+    Private Sub btnFind1_Click(sender As Object, e As EventArgs) Handles btnFind1.Click
+
+    End Sub
+
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+
+    End Sub
 End Class
